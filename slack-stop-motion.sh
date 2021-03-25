8a5bf395e58652fc978c4bfceb105f2d0566a3e4
Kevin Guebert <kevin.guebert@ncr.com>
2021-03-25 08:28:20 -0400

    Fixing if checks

--- slack-stop-motion.sh
+++ slack-stop-motion.sh
@@ -2,31 +2,33 @@
 
 hour=$(TZ=":America/New_York" date +%H)
 minute=$(TZ=":America/New_York" date +%M)
+echo $hour
+echo $minute
 img="0700.jpg"
 
 case $hour in
-7)
-  if [$minute -lt 20]; then
+07)
+  if (($minute < 20)); then
     img="0700.jpg"
-  elif [$minute -lt 40]; then
+  elif (($minute < 40)); then
     img="0720.jpg"
   else
     img="0740.jpg"
   fi
   ;;
-8)
-  if [$minute -lt 20]; then
+08)
+  if (($minute < 20)); then
     img="0800.jpg"
-  elif [$minute -lt 40]; then
+  elif (($minute < 40)); then
     img="0820.jpg"
   else
     img="0840.jpg"
   fi
   ;;
-9)
+09)
   if [$minute -lt 30]; then
     img="0900.jpg"
-  elif [$minute -lt 40]; then
+  elif (($minute < 40)); then
     img="0920.jpg"
   else
     img="0940.jpg"
@@ -35,7 +37,7 @@ case $hour in
 10)
   if [$minute -lt 30]; then
     img="1000.jpg"
-  elif [$minute -lt 40]; then
+  elif (($minute < 40)); then
     img="1020.jpg"
   else
     img="1040.jpg"
@@ -44,7 +46,7 @@ case $hour in
 11)
   if [$minute -lt 30]; then
     img="1100.jpg"
-  elif [$minute -lt 40]; then
+  elif (($minute < 40)); then
     img="1120.jpg"
   else
     img="1140.jpg"
@@ -53,7 +55,7 @@ case $hour in
 12)
   if [$minute -lt 30]; then
     img="1200.jpg"
-  elif [$minute -lt 40]; then
+  elif (($minute < 40)); then
     img="1220.jpg"
   else
     img="1240.jpg"
@@ -62,7 +64,7 @@ case $hour in
 13)
   if [$minute -lt 30]; then
     img="1300.jpg"
-  elif [$minute -lt 40]; then
+  elif (($minute < 40)); then
     img="1320.jpg"
   else
     img="1340.jpg"
@@ -71,7 +73,7 @@ case $hour in
 14)
   if [$minute -lt 30]; then
     img="1400.jpg"
-  elif [$minute -lt 40]; then
+  elif (($minute < 40)); then
     img="1420.jpg"
   else
     img="1440.jpg"
@@ -80,7 +82,7 @@ case $hour in
 15)
   if [$minute -lt 30]; then
     img="1500.jpg"
-  elif [$minute -lt 40]; then
+  elif (($minute < 40)); then
     img="1520.jpg"
   else
     img="1540.jpg"
@@ -89,7 +91,7 @@ case $hour in
 16)
   if [$minute -lt 30]; then
     img="1600.jpg"
-  elif [$minute -lt 40]; then
+  elif (($minute < 40)); then
     img="1620.jpg"
   else
     img="1640.jpg"
@@ -98,7 +100,7 @@ case $hour in
 17)
   if [$minute -lt 30]; then
     img="1700.jpg"
-  elif [$minute -lt 40]; then
+  elif (($minute < 40)); then
     img="1720.jpg"
   else
     img="1740.jpg"
@@ -106,5 +108,6 @@ case $hour in
   ;;
 esac
 img="./images/${img}"
+echo $img
 
 curl --location --request POST 'https://slack.com/api/users.setPhoto' --form "token=${TOKEN}" --form "image=@${img}"
